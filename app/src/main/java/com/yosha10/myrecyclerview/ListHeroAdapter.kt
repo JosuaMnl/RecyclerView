7554fe49c43c0ff85aba49056b9a57a44582c717
JosuaMnl <josuamnalu@gmail.com>
2023-03-03 17:06:43 +0700

    Apply View Binding

--- app/src/main/java/com/yosha10/myrecyclerview/ListHeroAdapter.kt
+++ app/src/main/java/com/yosha10/myrecyclerview/ListHeroAdapter.kt
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.yosha10.myrecyclerview.databinding.ItemRowHeroBinding
 
 class ListHeroAdapter(private val listHero: ArrayList<Hero>): RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
     private lateinit var onItemClickCallback: OnItemClickCallback
@@ -14,18 +15,19 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>): RecyclerView.Adapt
     fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback){
         this.onItemClickCallback = onItemClickCallback
     }
-    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
-        val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
-        val tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
+    class ListViewHolder(var binding: ItemRowHeroBinding) : RecyclerView.ViewHolder(binding.root) {
+//        val imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+//        val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+//        val tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
     }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ListHeroAdapter.ListViewHolder {
-        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
-        return ListViewHolder(view)
+        val binding = ItemRowHeroBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+//        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
+        return ListViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ListHeroAdapter.ListViewHolder, position: Int) {
@@ -33,9 +35,9 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>): RecyclerView.Adapt
         Glide.with(holder.itemView.context)
             .load(photo)
             .circleCrop()
-            .into(holder.imgPhoto)
-        holder.tvName.text = name
-        holder.tvDescription.text = description
+            .into(holder.binding.imgItemPhoto)
+        holder.binding.tvItemName.text = name
+        holder.binding.tvItemDescription.text = description
 //        holder.itemView.setOnClickListener {
 //            Toast.makeText(holder.itemView.context, "Kamu memilih ${listHero[holder.adapterPosition].name}", Toast.LENGTH_SHORT).show()
 //        }
