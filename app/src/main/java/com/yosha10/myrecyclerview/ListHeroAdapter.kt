9cdd4ac4ff8fda4398b8fe2bfcb36b5882edc14b
JosuaMnl <josuamnalu@gmail.com>
2023-03-03 16:32:41 +0700

    Initial Project

---
+++ app/src/main/java/com/yosha10/myrecyclerview/ListHeroAdapter.kt
@@ -0,0 +1,46 @@
+package com.yosha10.myrecyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ListHeroAdapter(private val listHero: ArrayList<Hero>): RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback){
+        this.onItemClickCallback = onItemClickCallback
+    }
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+        val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+        val tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ListHeroAdapter.ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListHeroAdapter.ListViewHolder, position: Int) {
+        val (name, description, photo) = listHero[position]
+        holder.imgPhoto.setImageResource(photo)
+        holder.tvName.text = name
+        holder.tvDescription.text = description
+//        holder.itemView.setOnClickListener {
+//            Toast.makeText(holder.itemView.context, "Kamu memilih ${listHero[holder.adapterPosition].name}", Toast.LENGTH_SHORT).show()
+//        }
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(listHero[holder.adapterPosition]) }
+    }
+
+    override fun getItemCount(): Int = listHero.size
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Hero)
+    }
+}
